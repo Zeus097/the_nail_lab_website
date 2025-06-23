8853eff8e0430ed73a71f043c2566ee0ad23eab0
Vasil Dimitrov <142613528+Zeus097@users.noreply.github.com>
2025-06-23 23:03:38 +0300

    Changed cover and info, because I will write down the site for the Nail Artist only

--- staff/models.py
+++ staff/models.py
@@ -12,11 +12,6 @@ class EmployeeBio(models.Model):
     biography = models.CharField(max_length=500)
     telephone_number = models.CharField(max_length=20)
     services = models.ManyToManyField('services.BaseService', related_name='employees')
-    # TODO: Да наследя тозиклас, като направя за всяка от 3те по един клас,
-    #  който да води към услугите на ('services.МАНИКЮР/ ГРИМ/ etc..'--> също наследяване и така да се записват в необходимите бази)
-
-    # TODO: След това ще трябва да оформя вече вютата и формите
-
 
     photo = models.ImageField(
         upload_to='staff_photos',
