b3548f3fd3fc7a313360737352d81659b97f62b4
Vasil Dimitrov <142613528+Zeus097@users.noreply.github.com>
2025-06-24 19:19:30 +0300

    Changed the profiles app name to user and added OneToOneField between EmployeeBio and User from Django contrib auth models

--- employee/models.py
+++ employee/models.py
@@ -1,8 +1,10 @@
+from django.contrib.auth.models import User
 from django.db import models
 from employee.validators import ImageSizeValidator
 
 
 class EmployeeBio(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     biography = models.CharField(max_length=500)
     telephone_number = models.CharField(max_length=20)
@@ -16,4 +18,4 @@ class EmployeeBio(models.Model):
     )
 
     def __str__(self):
-        return f"{self.name} ({self.job_type})"
+        return f"{self.name}"
