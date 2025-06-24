059a2c345b54ca8845569c44f5b7c2048369f277
Vasil Dimitrov <142613528+Zeus097@users.noreply.github.com>
2025-06-24 22:19:05 +0300

    Added properties for the ClientProfile for the email and the name, to get them from accounts app

--- user/models.py
+++ user/models.py
@@ -5,5 +5,15 @@ from django.conf import settings
 class ClientProfile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     phone = models.CharField(max_length=20)
-    name = models.CharField(max_length=20)
-    email = models.EmailField()
+
+    def __str__(self):
+        return f"{self.user.username} ({self.phone})"
+
+    @property
+    def email(self):
+        return self.user.email
+
+    @property
+    def name(self):
+        return self.user.username
+
