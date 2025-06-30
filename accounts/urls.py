377cd80f92ba161c49caa86b8b7bfdd66fb2e4a4
Vasil Dimitrov <142613528+Zeus097@users.noreply.github.com>
2025-06-30 17:47:47 +0300

    Added logic in services app- views[ServiceListView and the coresponding logic with fetching the data from the db model and rendered it in the template]. Also added TODOS in the template and got ride of the dogtag for multiline comments.

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,13 +9,13 @@ urlpatterns = [
     path('login/', LoginView.as_view(authentication_form=CustomLoginForm), name='login'),
     # path('', LogoutView.as_view(), name='logout'),
 
-    """
-        TODO: Add logout logic and make the button inside the profile menu
-        
-        TODO: Read from lecturer for logout with method POST in GitHub forumApp
-            and the style none to be added in separate CSS in the logout.css, when I crate it!
-
-        TODO: It has it on screenshot tho, so I can do it in the file for logout.html..!
-    """
+        #
+        # TODO: Add logout logic and make the button inside the profile menu
+        #
+        # TODO: Read from lecturer for logout with method POST in GitHub forumApp
+        #     and the style none to be added in separate CSS in the logout.css, when I crate it!
+        #
+        # TODO: It has it on screenshot tho, so I can do it in the file for logout.html..!
+        #
 
 ]
--- services/views.py
+++ services/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from services.models import BaseService
 
 
 class ServiceListView(ListView):
-    pass
+    context_object_name = 'services'
+    model = BaseService
+    template_name = 'services/services-page.html'
+
+    def get_queryset(self):
+        queryset = self.model.objects.all()
+        return queryset.order_by('id')
