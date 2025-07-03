ee2213ef8f73000681b931b24b75ede2e4f21d79
Vasil Dimitrov <142613528+Zeus097@users.noreply.github.com>
2025-07-03 23:55:39 +0300

    Removed the completed TODOs from the urls.py from the accounts app

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,14 +8,4 @@ urlpatterns = [
     path('registration/', views.UserRegistrationView.as_view(), name='register'),
     path('login/', LoginView.as_view(authentication_form=CustomLoginForm), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-
-        #
-        # TODO: Add logout logic and make the button inside the profile menu
-        #
-        # TODO: Read from lecturer for logout with method POST in GitHub forumApp
-        #     and the style none to be added in separate CSS in the logout.css, when I crate it!
-        #
-        # TODO: It has it on screenshot tho, so I can do it in the file for logout.html..!
-        #
-
 ]
