9f310822768e13414a4697cbcc0265b256b2e3e5
Vasil Dimitrov <142613528+Zeus097@users.noreply.github.com>
2025-07-13 14:17:08 +0300

    Update on gallery-main page style visualisation

--- photos/models.py
+++ photos/models.py
@@ -2,8 +2,16 @@ from django.db import models
 
 
 class GalleryPhoto(models.Model):
-    name = models.CharField(max_length=50)
-    description = models.CharField(max_length=200)
+    name = models.CharField(
+        max_length=50,
+        null=True,
+        blank=True,
+    )
+    description = models.CharField(
+        max_length=200,
+        null=True,
+        blank=True
+    )
     upload_date = models.DateTimeField(auto_now_add=True)
     photo = models.ImageField(upload_to='gallery_photos',)
 
