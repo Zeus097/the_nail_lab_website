7bb3d1d19315ba31ddde6f459a2f71379188cb1b
Vasil Dimitrov <142613528+Zeus097@users.noreply.github.com>
2025-07-13 23:43:47 +0300

    Updated utils.py in appointments app -to skip search for past days.

--- appointments/utils.py
+++ appointments/utils.py
@@ -1,4 +1,7 @@
 from datetime import datetime, timedelta
+from math import ceil
+
+from django.utils.timezone import localtime
 from appointments.models import Appointment, DayOff
 from appointments.config import WORK_START, WORK_END, INTERVAL, DAYS_AHEAD
 
@@ -6,16 +9,30 @@ from appointments.config import WORK_START, WORK_END, INTERVAL, DAYS_AHEAD
 def find_earliest_available_slots(employee, service, start_date, max_slots=5):
     slots = []
     duration = timedelta(minutes=service.duration)
-    date = start_date
+    today = localtime().date()
+    now_time = localtime().time()
+
+    if start_date < today:
+        return []
 
     for day in range(DAYS_AHEAD):
-        current_date = date + timedelta(days=day)
+        current_date = start_date + timedelta(days=day)
 
         if DayOff.objects.filter(employee=employee, date=current_date).exists():
             continue
 
+        if current_date < today:
+            continue
+
         appointments = Appointment.objects.filter(employee=employee, date=current_date)
         start_dt = datetime.combine(current_date, WORK_START)
+
+        if current_date == today and now_time > WORK_START:
+            current_dt = localtime()
+            minute = ceil(current_dt.minute / (INTERVAL.seconds // 60)) * (INTERVAL.seconds // 60)
+            rounded = current_dt.replace(minute=0, second=0, microsecond=0) + timedelta(minutes=minute)
+            start_dt = rounded
+
         end_dt = datetime.combine(current_date, WORK_END) - duration
 
         while start_dt <= end_dt:
