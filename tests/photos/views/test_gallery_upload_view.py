291e143eb1e621f40a2bb8a867701814c7d50635
Vasil Dimitrov <142613528+Zeus097@users.noreply.github.com>
2025-07-26 11:50:36 +0300

    add tests for photos app- testing gallery views

---
+++ tests/photos/views/test_gallery_upload_view.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
+
+
+class TestGalleryUploadView(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='ivan', email='ivan@test.com', password='asd123')
+        self.client.login(username='ivan', email='ivan@test.com', password='asd123')
+
+    def test_get_request_returns_form(self):
+        response = self.client.get('/photos/gallery/upload/')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'photos_gallery/gallery_upload.html')
+        self.assertIn('form', response.context)
