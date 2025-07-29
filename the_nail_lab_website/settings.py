53a49d56ff8957263bb6d0925b67dd3bfc621cfb
Vasil Dimitrov <142613528+Zeus097@users.noreply.github.com>
2025-07-29 18:58:26 +0300

    Fix ALLOWED_HOSTS via .env and python-decouple

--- the_nail_lab_website/settings.py
+++ the_nail_lab_website/settings.py
@@ -32,18 +32,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY')
 if not SECRET_KEY:
     raise RuntimeError("SECRET_KEY environment variable not set")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'False').lower() in ('true', '1', 'yes')
+DEBUG = config('DEBUG', default='False').lower() in ('true', '1', 'yes')
 
 
 
 
-# ALLOWED_HOSTS = [host.strip() for host in config('ALLOWED_HOSTS', default='').split(',') if host.strip()]
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [host.strip() for host in config('ALLOWED_HOSTS', default='').split(',') if host.strip()]
 
 
 
