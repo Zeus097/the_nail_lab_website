0a6e9bbbbae1a2fa1f0d000e103d54a3dae1bb37
Vasil Dimitrov <142613528+Zeus097@users.noreply.github.com>
2025-08-01 22:25:15 +0300

    Fix Supabase media backend placement

--- the_nail_lab_website/settings.py
+++ the_nail_lab_website/settings.py
@@ -48,6 +48,8 @@ PROJECT_APPS = [
     'photos',
     'appointments',
     'storages',  # Required for django-storages
+    'cloudinary',
+    'cloudinary_storage',
 ]
 
 INSTALLED_APPS = [
@@ -254,6 +256,16 @@ SOCIAL_AUTH_PIPELINE = [
     'social_core.pipeline.user.user_details',
 ]
 
+# CLOUDINARY FOR media storage
+# =================================
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': config('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': config('CLOUDINARY_API_KEY'),
+    'API_SECRET': config('CLOUDINARY_API_SECRET'),
+}
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 
 
