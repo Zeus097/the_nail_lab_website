bd2aef1919ef624cceca84edf1632a98a8e73642
Vasil Dimitrov <142613528+Zeus097@users.noreply.github.com>
2025-08-01 07:06:56 +0300

    changed a variable name for the DB in the secret file

--- the_nail_lab_website/settings.py
+++ the_nail_lab_website/settings.py
@@ -282,10 +282,10 @@ SOCIAL_AUTH_STRATEGY = 'social_django.strategy.DjangoStrategy'
 if ENVIRONMENT == "production":
     DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
-    AWS_ACCESS_KEY_ID = config("SUPABASE_PUBLIC_ANON_KEY")
-    AWS_SECRET_ACCESS_KEY = config("SUPABASE_PUBLIC_ANON_KEY")  # за Supabase, key = secret
-    AWS_STORAGE_BUCKET_NAME = config("SUPABASE_BUCKET")
-    AWS_S3_ENDPOINT_URL = f"{config('SUPABASE_URL')}/storage/v1"
+    AWS_ACCESS_KEY_ID = os.environ.get("SUPABASE_PUBLIC_ANON_KEY")
+    AWS_SECRET_ACCESS_KEY = os.environ.get("SUPABASE_PUBLIC_ANON_KEY")  # за Supabase, key = secret
+    AWS_STORAGE_BUCKET_NAME = os.environ.get("SUPABASE_BUCKET")
+    AWS_S3_ENDPOINT_URL = f"{os.environ.get('SUPABASE_URL')}/storage/v1"
 
     AWS_S3_FILE_OVERWRITE = False
     AWS_DEFAULT_ACL = None
