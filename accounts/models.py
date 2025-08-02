4751c7eff05ae93d07a9753d6dfa74bbd0f7ce1d
Vasil Dimitrov <142613528+Zeus097@users.noreply.github.com>
2025-08-02 03:10:49 +0300

    Fixed the phone validator logic

--- accounts/models.py
+++ accounts/models.py
@@ -29,6 +29,17 @@ class BaseUser(AbstractUser):
         validators=[ImageSizeValidator(5)],
     )
 
+    # Saves ph. number without spaces.
+    def clean(self):
+        super().clean()
+        if self.telephone_number:
+            self.telephone_number = self.telephone_number.replace(
+                ' ', ''
+            ).replace(
+                '\t', ''
+            ).strip()
+
+
 
     def __str__(self):
         return self.username
--- accounts/validators.py
+++ accounts/validators.py
@@ -28,14 +28,16 @@ class ImageSizeValidator:
 
 @deconstructible
 class PhoneValidator:
-    phone_regex = re.compile(r'^\+?\d{9,15}$')
 
-    message = "Телефонният номер трябва да съдържа 9 до 15 цифри и може да започва с '+'."
+    # Allowss ph. numbers between 9 and 5 diggits, optional starting with '+'
+    phone_regex = re.compile(r'^\+?\d{9,15}$')
+    message = "Телефонният номер трябва да съдържа от 9 до 15 цифри и може да започва с '+'."
     code = 'invalid_phone'
 
     def __call__(self, value):
-        normalized = value.replace(' ', '')
-        # case if user enter ph. number with spaces
+
+        # Remove spaces
+        normalized = value.replace(' ', '').replace('\t', '')
 
         if not self.phone_regex.match(normalized):
             raise ValidationError(self.message, code=self.code)
