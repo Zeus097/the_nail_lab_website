dba29de6692ba2805b6105b833810a17798ea9ee
Vasil Dimitrov <142613528+Zeus097@users.noreply.github.com>
2025-08-04 13:42:22 +0300

    Update signals.py

--- accounts/signals.py
+++ accounts/signals.py
@@ -26,3 +26,29 @@ def sync_user_profiles(sender, instance, created, **kwargs):
 
     if instance.is_client:
         ClientProfile.objects.get_or_create(user=instance)
+
+
+
+# # WORK IN PRODUCTION
+# # signal for deleting old profile picture in Cloudinary, after changing
+
+# @receiver(pre_save, sender=BaseUser)
+# def delete_old_photo_if_changed(sender, instance, **kwargs):
+#     if not instance.pk:
+#         return
+
+#     try:
+#         old_instance = BaseUser.objects.get(pk=instance.pk)
+#     except BaseUser.DoesNotExist:
+#         return
+
+#     old_photo = old_instance.photo
+#     new_photo = instance.photo
+
+
+#     if old_photo and old_photo != new_photo:
+#         try:
+#             public_id = old_photo.name.rsplit('.', 1)[0]
+#             cloudinary.uploader.destroy(public_id)
+#         except Exception as e:
+#             print(f"Cloudinary deletion error: {e}")
